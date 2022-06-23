bc0869521d76e60a5c911557ca9f88f9efdda19c
GretaMaciul <gretamaciul@gmail.com>
2022-06-23 15:12:07 +0100

    Practicing JavaScript
    Basic guess the number game

--- HTML Essentials/javabasic.js
+++ HTML Essentials/javabasic.js
@@ -1,28 +1,8 @@
-console.log("Hello! I'm not sure what I am doing")
+const password = prompt("Please enter password");
 
-if (1 + 1 === 2) {
-    console.log("Doing math")
+if (password.indexOf(' ') === -1) {
+    console.log("Well done for no spaces")
 }
-
-let random = Math.random();
-if (random < 0.5) {
-    console.log("Your number is less than 0.5!")
-    console.log(random);
-}
-
 else {
-    console.log("Your number is greater than or equal to 0.5!")
-}
-
-const dayOfWeek = prompt('Enter day of the week').toLowerCase();
-
-if (dayOfWeek === 'Monday') {
-    console.log("I hate Mondays")
-}
-else if (dayOfWeek === 'Saturday') {
-    console.log("I love Saturdays")
-}
-
-else {
-    console.log("Meh, it's okay I guess")
+    console.log("Booo! Spaces icky")
 }
